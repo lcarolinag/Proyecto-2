e0f279081aeb36c493e785df700c1d845e73628e
Lennys Carolina <121388716+lcarolinag@users.noreply.github.com>
2023-07-12 08:33:17 -0300

    Add files via upload

---
+++ Proyecto-2/selectores.js
@@ -0,0 +1,12 @@
+let h1Element = document.querySelector("h1");
+h1Element.style.color = "red";
+
+var button = document.getElementById("miCv");
+
+
+button.addEventListener("click", function(){
+ alert("Haz hecho click en el boton!");
+});
+
+
+
